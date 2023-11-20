cba66e8caa27dd9222bba53329f6cb9e037f61c6
dhilshana <dhldhilshana@gmail.com>
2023-11-20 22:15:41 +0530

    login page change

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:ui';
+
 import 'package:eventlister/pages/signuppage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -47,18 +50,32 @@ class _LoginPageState extends State<LoginPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
+              style: TextStyle(color: Color.fromARGB(255, 154, 56, 171)),
               controller: emailController,
               decoration: InputDecoration(
-                labelText: 'Email',
-              ),
+                  labelText: 'Email',
+                  labelStyle:
+                      TextStyle(color: Color.fromARGB(255, 154, 56, 171)),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 154, 56, 171)))),
             ),
             SizedBox(height: 16.0),
             TextField(
+              style: TextStyle(color: Color.fromARGB(255, 154, 56, 171)),
               controller: passwordController,
               obscureText: true,
               decoration: InputDecoration(
-                labelText: 'Password',
-              ),
+                  labelText: 'Password',
+                  labelStyle:
+                      TextStyle(color: Color.fromARGB(255, 154, 56, 171)),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 154, 56, 171))),
+                  suffixIcon: new Icon(Icons.remove_red_eye,
+                    
+                  ),
+                  ),
             ),
             SizedBox(height: 8.0),
             if (errorMessage != null)
@@ -66,19 +83,47 @@ class _LoginPageState extends State<LoginPage> {
                 errorMessage!,
                 style: TextStyle(color: Colors.red),
               ),
-            SizedBox(height: 24.0),
-            ElevatedButton(
-              onPressed: () {
-                signIn();
-              },
-              child: Text('Login'),
+            SizedBox(
+              height: 24.0,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  signIn();
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Color.fromARGB(255, 154, 56, 171),
+                  ),
+                ),
+                child: Text('Login',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 17,
+                    )),
+              ),
+            ),
+            SizedBox(
+              height: 15.0,
             ),
             TextButton(
                 onPressed: () {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => SignUp()));
                 },
-                child: Text('Register Now'))
+                style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                  Color.fromARGB(255, 154, 56, 171),
+                )),
+                child: Text(
+                  'Register Now',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 17,
+                  ),
+                )),
           ],
         ),
       ),
