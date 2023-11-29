79a985f85412fb0536cf2f94331f4cd3d912e7dd
jasimrazi <jasimrazi17@gmail.com>
2023-11-29 13:47:53 +0530

    Test adding to DB

--- lib/pages/createpage.dart
+++ lib/pages/createpage.dart
@@ -1,5 +1,6 @@
 import 'package:eventlister/pages/homepage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class CreatePage extends StatefulWidget {
@@ -12,6 +13,11 @@ class CreatePage extends StatefulWidget {
 class _CreatePageState extends State<CreatePage> {
   final user = FirebaseAuth.instance.currentUser;
 
+  final TextEditingController eventNameController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController venueController = TextEditingController();
+  final TextEditingController regLinkController = TextEditingController();
+
   // Method to handle sign out
   Future signOut() async {
     await FirebaseAuth.instance.signOut();
@@ -20,6 +26,27 @@ class _CreatePageState extends State<CreatePage> {
       '/home', // Use the route name for your homepage
       (route) => false, // Remove all routes from the stack
     );
+  }
+
+  //Method to add events
+
+  Future addEvent(String eventName, String description, String venue,
+      String regLink) async {
+    await FirebaseFirestore.instance.collection('Events').add({
+      'Event name': eventName,
+      'Description': description,
+      'Venue': venue,
+      'Link': regLink,
+    });
+  }
+
+  @override
+  void dispose() {
+    eventNameController.dispose();
+    descriptionController.dispose();
+    venueController.dispose();
+    regLinkController.dispose();
+    super.dispose();
   }
 
   @override
@@ -34,6 +61,57 @@ class _CreatePageState extends State<CreatePage> {
           children: [
             Text("Signed in as: $userEmail"),
             SizedBox(height: 20),
+            TextField(
+              controller: eventNameController,
+              decoration: InputDecoration(
+                hintText: 'Enter Event Name',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Description',
+              style: TextStyle(fontSize: 18),
+            ),
+            TextField(
+              controller: descriptionController,
+              decoration: InputDecoration(
+                hintText: 'Enter Description',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Venue',
+              style: TextStyle(fontSize: 18),
+            ),
+            TextField(
+              controller: venueController,
+              decoration: InputDecoration(
+                hintText: 'Enter Venue',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Registration Link',
+              style: TextStyle(fontSize: 18),
+            ),
+            TextField(
+              controller: regLinkController,
+              decoration: InputDecoration(
+                hintText: 'Enter Registration Link',
+              ),
+            ),
+            Spacer(),
+            ElevatedButton(
+                onPressed: () {
+                  // Implement your save logic here
+                  addEvent(
+                    eventNameController.text.trim(),
+                    descriptionController.text.trim(),
+                    venueController.text.trim(),
+                    regLinkController.text.trim(),
+                  );
+                },
+                child: Text('Save')),
             TextButton(
               onPressed: signOut,
               child: Text("Log Out"),
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -75,7 +75,7 @@ class _HomePageState extends State<HomePage> {
                                   ),
                                   image: DecorationImage(
                                     image: NetworkImage(
-                                      "https://w0.peakpx.com/wallpaper/138/244/HD-wallpaper-gray-and-black-laptop-computer-on-surface.jpg",
+                                      "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                                     ),
                                     fit: BoxFit.cover,
                                   ),
